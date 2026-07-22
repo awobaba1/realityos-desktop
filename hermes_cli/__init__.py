1911c78fe2873481af6bca4e862837ecace3f2c2
吴刚 <wugang@wugangdeMacBook-Air-5.local>
2026-07-23 00:19:48 +0800

    chore: bump version to v0.18.6 (2026.7.28)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.18.5"
-__release_date__ = "2026.7.27"
+__version__ = "0.18.6"
+__release_date__ = "2026.7.28"
 
 
 def _ensure_utf8():
